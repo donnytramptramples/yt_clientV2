f0629cecd5f350f0bb9f50e4bbce11baf0f5bd64
donnytramptramples <146447552+donnytramptramples@users.noreply.github.com>
2026-04-12 14:39:13 +0800

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,13 +1,14 @@
 @echo off
 cd /d "%~dp0"
-echo [Checking GitHub...]
+echo [UPDATER] Checking GitHub...
 git fetch
 git status -uno | findstr /C:"behind" > nul
 if %errorlevel% == 0 (
-    echo [Update Found! Pulling...]
+    echo [UPDATER] New code found! Updating...
     git pull origin main
     call npm install
+    echo [UPDATER] Restarting engine...
     taskkill /F /IM node.exe
 ) else (
-    echo [No updates found. Everything is current.]
+    echo [UPDATER] No changes found.
 )
