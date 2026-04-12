8a3853fa624420545a0653ac10a1aa23213efd70
donnytramptramples <146447552+donnytramptramples@users.noreply.github.com>
2026-04-12 14:34:35 +0800

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,9 +1,13 @@
 @echo off
 cd /d "%~dp0"
+echo [Checking GitHub...]
 git fetch
 git status -uno | findstr /C:"behind" > nul
 if %errorlevel% == 0 (
+    echo [Update Found! Pulling...]
     git pull origin main
     call npm install
     taskkill /F /IM node.exe
+) else (
+    echo [No updates found. Everything is current.]
 )
