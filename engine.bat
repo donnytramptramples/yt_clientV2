8cb0cc87e1aa914aca3b9d53c5f871a253e5c6f4
donnytramptramples <146447552+donnytramptramples@users.noreply.github.com>
2026-04-12 14:39:27 +0800

    Update server restart message in engine.bat

--- engine.bat
+++ engine.bat
@@ -1,7 +1,9 @@
 @echo off
 cd /d "%~dp0"
 :loop
+cls
+echo [ENGINE] Starting server...
 node server.js
-echo Server stopped. Restarting in 10s...
+echo [ENGINE] Server stopped/crashed. Restarting in 10s...
 timeout /t 10
 goto loop
